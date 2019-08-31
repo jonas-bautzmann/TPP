18356bca8083df8a783d2ae7e94a3d5948d4c0a4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-31 12:42:49 +0000

    Deploy joba-sprd/TPP to github.com/joba-sprd/TPP.git:gh-pages

--- src/TagInput.ts
+++
@@ -1,7 +0,0 @@
-import * as $ from "jquery";
-
-const body = $("body");
-
-export const toggleTagInput = () => body.toggleClass("hide-tag-input");
-
-export const hideTagInput = () => body.addClass("hide-tag-input");
